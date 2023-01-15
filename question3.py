1eb3940eebe1200e7c13587d09585ce3ee7db365
Sung Hyuck Hong <shong16@kaist.ac.kr>
2023-01-16 08:04:16 +0900

    Cross validation of four different classifiers

--- question3.py
+++ question3.py
@@ -41,16 +41,27 @@ data['Class Duration']=data['Class Duration'].apply(convert_range_into_num)
 # Construct test and training sets
 X=data.drop('Adaptivity Level_n',axis='columns')
 Y=data['Adaptivity Level_n']
+
+#Model Selection
+from sklearn.model_selection import cross_val_score
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+
+print("Result of Cross Validation: Decision Tree / Random Forest /Support Vector / K Neighbors Classifier")
+print(cross_val_score(tree.DecisionTreeClassifier(),X,Y))
+print(cross_val_score(RandomForestClassifier(),X,Y))
+print(cross_val_score(SVC(),X,Y))
+print(cross_val_score(KNeighborsClassifier(n_neighbors=3),X,Y))
+
+
+#Splitting training and test dataset
 from sklearn.model_selection import train_test_split
 X_train, X_test, Y_train, Y_test=train_test_split(X,Y,test_size=0.2,random_state=10)
 
-#Model 1: Decision Tree Model
-from sklearn import tree
-decision_tree_model=tree.DecisionTreeClassifier()
-decision_tree_model.fit(X_train,Y_train)
 
-#Model 2:
-from sklearn.ensemble import RandomForestClassifier
+#Model training:
 random_forest_model=RandomForestClassifier()
 random_forest_model.fit(X_train,Y_train)
 
@@ -58,26 +69,9 @@ random_forest_model.fit(X_train,Y_train)
 #Performance Evaluation
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import classification_report
-print("Result 1: Decision Tree")
-print("Score: %f" % decision_tree_model.score(X_test,Y_test))
-predictions_decision_tree=decision_tree_model.predict(X_test)
-print("Confusion Matrix:")
-print(confusion_matrix(Y_test,predictions_decision_tree))
-print("Classification Report: ")
-print(classification_report(Y_test,predictions_decision_tree, target_names=['High', 'Low', 'Moderate']))
-
-plt.subplot(211)
-feature_label=['Age', 'Class Duration', 'Gender', 'Edu. Level',
-       'Inst. Type', 'IT Student', 'Location', 'Load-shedding',
-       'Fin. Condition', 'Internet Type', 'Network Type',
-       'Self Lms', 'Device']
-plt.bar(feature_label, decision_tree_model.feature_importances_)
-plt.title('Feature Importance: Decision Tree')
 
 
-
-
-print("Result 2: Random Forest")
+print("Result 1: Random Forest")
 print("Score: %f" % random_forest_model.score(X_test,Y_test))
 predictions_random_forest=random_forest_model.predict(X_test)
 print("Confusion Matrix:")
@@ -85,10 +79,15 @@ print(confusion_matrix(Y_test,predictions_random_forest))
 print("Classification Report: ")
 print(classification_report(Y_test,predictions_random_forest, target_names=['High', 'Low', 'Moderate']))
 
-plt.subplot(212)
+plt.figure(1)
+feature_label=['Age', 'Class Duration', 'Gender', 'Edu. Level',
+       'Inst. Type', 'IT Student', 'Location', 'Load-shedding',
+       'Fin. Condition', 'Internet Type', 'Network Type',
+       'Self Lms', 'Device']
 plt.bar(feature_label, random_forest_model.feature_importances_)
 plt.title('Feature Importance: Random Forest')
 plt.show()
+
 
 #histogram of columns of interest
 
@@ -98,8 +97,5 @@ plt.show()
 #plt.ylabel("Count")
 #plt.title('Age Histogram')
 #plt.show()
-
-
-
 
 
